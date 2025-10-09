a854b3cc0323276b71bfc6e38843148ea42a54b1
Théo GRILLON <theogrillon6f9@gmail.com>
2025-10-09 16:26:52 +0200

    Config update

--- App/Include/Application/Primitive.h
+++ App/Include/Application/Primitive.h
@@ -1,31 +1,45 @@
 #pragma once
 
-#include "glm/glm.hpp"
+#include "Application/BaseType.h"
 
 #include <array>
 
+/// @brief Helper namespace for index manipulations in triangular faces.
+namespace IndexHelpers
+{
+constexpr std::array<uint8_t, 3> Previous{ 2, 0, 1 };
+constexpr std::array<uint8_t, 3> Current{ 0, 1, 2 };
+constexpr std::array<uint8_t, 3> Next{ 1, 2, 0 };
+} // namespace IndexHelpers
+
 namespace Data::Primitive
 {
-struct Triangle;
+struct Face;
 
 /// @brief Structure holding the informations relative to a vertex primitive.
 /// @note IncidentFace is a pointer to one of the incident faces of the vertex.
 struct Vertex
 {
+	/// @brief Unique index of the vertex in the mesh.
+	BaseType::IndexType Index;
+
 	/// @brief Vertex position in 3D.
-	glm::vec3 Position;
+	BaseType::Vec3 Position;
 
 	/// @brief Pointer to one of the incident faces for internal purposes.
-	Triangle* IncidentFace;
+	Face* IncidentFace;
 };
 
 /// @brief  Structure holding the informations relative to a triangular face primitive.
-struct Triangle
+struct Face
 {
-	/// @brief Triangle vertices.
+	/// @brief Unique index of the triangle in the mesh.
+	BaseType::IndexType Index;
+
+	/// @brief Face vertices.
 	std::array<Vertex*, 3> Vertices;
 
-	/// @brief Triangle neighboring faces
-	std::array<Triangle*, 3> Neighbors;
+	/// @brief Face neighboring faces
+	std::array<Face*, 3> Neighbors;
 };
 } // namespace Data::Primitive
--- App/Test/Source/MeshLoader_utest.cpp
+++ App/Test/Source/MeshLoader_utest.cpp
@@ -8,6 +8,21 @@ TEST(MeshLoaderTest, LoadOFF_ValidFile_ShouldLoadMesh)
 {
 	std::unique_ptr<Data::Surface::Mesh> mesh = MeshLoader::LoadOFF("TestFiles/Off/cube.off");
 	ASSERT_NE(mesh, nullptr);
+	glm::vec3 p;
+
 	EXPECT_EQ(mesh->GetVertexCount(), 8);
 	EXPECT_EQ(mesh->GetFaceCount(), 12);
+}
+
+TEST(MeshLoaderTest, LoadOFF_InvalidFile_ShouldReturnNullptr)
+{
+	{ // Wrong file extension.
+		std::unique_ptr<Data::Surface::Mesh> mesh = MeshLoader::LoadOFF("TestFiles/Obj/cube.obj");
+		EXPECT_EQ(mesh, nullptr);
+	}
+
+	{ // Can't open file.
+		std::unique_ptr<Data::Surface::Mesh> mesh = MeshLoader::LoadOFF("TestFiles/Off/notAFile.off");
+		EXPECT_EQ(mesh, nullptr);
+	}
 }
