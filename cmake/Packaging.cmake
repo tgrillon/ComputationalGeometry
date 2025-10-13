b7e16c7667866bdb0a5c1a4118265efd9ed667b3
Théo GRILLON <theogrillon6f9@gmail.com>
2025-10-13 09:40:38 +0200

    Update Packaging.cmake

--- cmake/Packaging.cmake
+++ cmake/Packaging.cmake
@@ -1,5 +1,5 @@
 # CPack configuration
 set(CPACK_PACKAGE_VENDOR "Théo GRILLON")
 set(CPACK_PACKAGE_CONTACT "theogrillon6f9@gmail.com")
-set(CPACK_PACKAGE_DESCRIPTION "GAM")
+set(CPACK_PACKAGE_DESCRIPTION "Template for C++ projects using CMake")
 include(CPack)
