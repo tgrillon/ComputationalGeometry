81f74ae8a605e0f7b5fd77041180e9e56282aad9
tgrillon <tgrillon@users.noreply.github.com>
2025-10-13 08:13:19 +0000

    deploy: 33fcf1d965ef6b6dd55b474aa62ea76a9f8764d6

---
+++ memcheck/report.memcheck.html.part.js
@@ -0,0 +1,23 @@
+async function updateContentOnceLoaded1()
+{
+    var data =`
+==6297== Memcheck, a memory error detector<br />
+==6297== Copyright (C) 2002-2022, and GNU GPL'd, by Julian Seward et al.<br />
+==6297== Using Valgrind-3.22.0 and LibVEX; rerun with -h for copyright info<br />
+==6297== Command: /home/runner/work/SandBox/SandBox/build/App/Test/AppUnitTests<br />
+==6297== Parent PID: 6288<br />
+==6297== <br />
+==6297== <br />
+==6297== <span class="valgrind_summary_title">HEAP SUMMARY:</span><br />
+==6297== &nbsp; &nbsp; in use at exit: 0 bytes in 0 blocks<br />
+==6297== &nbsp; total heap usage: 532 allocs, 532 frees, 201,765 bytes allocated<br />
+==6297== <br />
+==6297== All heap blocks were freed -- no leaks are possible<br />
+==6297== <br />
+==6297== For lists of detected and suppressed errors, rerun with: -s<br />
+==6297== <span class="valgrind_summary_title">ERROR SUMMARY:</span> 0 errors from 0 contexts (suppressed: 0 from 0)<br />
+`;
+    var analysis_div = document.getElementById('valgrind.result1.Report');
+    analysis_div.innerHTML=data;
+}
+updateContentOnceLoaded1();
