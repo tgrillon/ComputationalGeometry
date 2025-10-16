066f116b668dcf045911f65d181d4bbbb8175729
Théo GRILLON <theogrillon6f9@gmail.com>
2025-10-16 15:01:22 +0200

    Add CreateGridMesh TestHelpers function

--- App/Test/Include/TestHelpers.h
+++ App/Test/Include/TestHelpers.h
@@ -5,6 +5,7 @@
 
 namespace TestHelpers
 {
+/// @brief Create a valid mesh with 4 vertices and 2 faces, and add extra data to vertices.
 inline Data::Surface::Mesh CreateValidMesh()
 {
 	Data::Surface::Mesh mesh;
@@ -22,6 +23,7 @@ inline Data::Surface::Mesh CreateValidMesh()
 	return mesh;
 }
 
+/// @brief Create a valid mesh with extra data for each vertex.
 inline Data::Surface::Mesh CreateValidMeshWithED()
 {
 	Data::Surface::Mesh mesh;
@@ -56,6 +58,53 @@ inline Data::Surface::Mesh CreateValidMeshWithED()
 	// Add faces
 	mesh.AddFace({ .Vertices = { 0, 1, 2 }, .Neighbors = { -1, 1, -1 } });
 	mesh.AddFace({ .Vertices = { 0, 2, 3 }, .Neighbors = { -1, -1, 0 } });
+
+	// Add extra data containers for each face.
+	mesh.AddFacesExtraDataContainer();
+
+	// Get Face proxies to add extra data.
+	Data::Primitive::FaceProxy f0 = mesh.GetFace(0);
+	Data::Primitive::FaceProxy f1 = mesh.GetFace(1);
+
+	// Add face normals
+	f0.GetOrCreateExtraData<BaseType::Vec3>() = { 0, 0, 1 };
+	f1.GetOrCreateExtraData<BaseType::Vec3>() = { 0, 0, 1 };
+
+	return mesh;
+}
+
+/// @brief Create a grid mesh with (nRow+1)*(nCol+1) vertices and 2*nRow*nCol faces.
+inline Data::Surface::Mesh CreateGridMesh(int nRow = 1, int nCol = 1)
+{
+	Data::Surface::Mesh mesh;
+
+	for(int iRow = 0; iRow <= nRow; ++iRow)
+	{
+		float y = static_cast<float>(iRow);
+		for(int iCol = 0; iCol <= nCol; ++iCol)
+		{
+			// Add vertex
+			float x = static_cast<float>(iCol);
+			mesh.AddVertex({ .Position = { x, y, 0 } });
+
+			// Add faces
+			if(iRow > 0 && iCol > 0)
+			{
+				int prevRow = iRow - 1;
+				int prevCol = iCol - 1;
+				int nVertexCol = nCol + 1;
+				mesh.AddFace({ .Vertices = { prevRow * nVertexCol + prevCol,
+											 prevRow * nVertexCol + iCol,
+											 iRow * nVertexCol + iCol } });
+				mesh.AddFace({ .Vertices = { prevRow * nVertexCol + prevCol,
+											 iRow * nVertexCol + iCol,
+											 iRow * nVertexCol + prevCol } });
+			}
+		}
+	}
+
+	// Update mesh connectivity (neighbors and incident faces)
+	mesh.UpdateMeshConnectivity();
 
 	return mesh;
 }
