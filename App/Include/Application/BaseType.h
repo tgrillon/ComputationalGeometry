ca5300b9bc022d4342b633ec58514915e8560436
Théo GRILLON <theogrillon6f9@gmail.com>
2025-10-20 16:46:41 +0200

    Add std::hash definition for vec22 and vec3

--- App/Include/Application/BaseType.h
+++ App/Include/Application/BaseType.h
@@ -17,3 +17,31 @@ using Vec2 = glm::vec2;
 using Vec3 = glm::vec3;
 using Vec4 = glm::vec4;
 } // namespace BaseType
+
+namespace std
+{
+/// Hash function for Vec2 to be used in unordered containers.
+template<>
+struct hash<BaseType::Vec2>
+{
+	size_t operator()(const BaseType::Vec2& elt) const
+	{
+		auto h1 = std::hash<BaseType::VertexIndex>{}(elt.x);
+		auto h2 = std::hash<BaseType::VertexIndex>{}(elt.y);
+		return h1 ^ h2;
+	}
+};
+
+/// Hash function for Vec3 to be used in unordered containers.
+template<>
+struct hash<BaseType::Vec3>
+{
+	size_t operator()(const BaseType::Vec3& elt) const
+	{
+		auto h1 = std::hash<BaseType::VertexIndex>{}(elt.x);
+		auto h2 = std::hash<BaseType::VertexIndex>{}(elt.y);
+		auto h3 = std::hash<BaseType::VertexIndex>{}(elt.z);
+		return h1 ^ h2 ^ h3;
+	}
+};
+} // namespace std
